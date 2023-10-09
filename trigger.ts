9de1ca89027a146405a3a3d75fc68e56535bbe70
Matt Gabrenya <matt@buildyourweb.app>
2023-10-08 18:42:31 -0700

    fix: build with nix command

--- trigger.ts
+++ trigger.ts
@@ -3,9 +3,13 @@ import {Octokit} from "octokit";
 import 'dotenv/config';
 
 const RELEASE_NAME = process.argv[3];
-const RELEASE_REF = `tags/${RELEASE_NAME}`;
 
 async function main() {
+  if(!RELEASE_NAME) {
+    console.error("Error: Set a release name");
+    process.exit();
+  }
+
   const octokit = new Octokit({
     auth: process.env.GITHUB_AUTH_TOKEN
   })
@@ -13,18 +17,19 @@ async function main() {
   try {
     let res = await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
       owner: 'buildyourwebapp',
-      repo: 'holochain-prebuild-binaries',
+      repo: 'holochain-prebuilt-binaries',
       event_type: 'new_holochain_release',
       client_payload: {
-        ref: RELEASE_REF
+        ref: RELEASE_NAME
       },
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
       }
     });
+    console.log("Triggered build-release workflow");
   } catch(e) {
     //@ts-ignore
-    console.error(`Error: **${(e as AxiosError).response.statusText}** ${(e as AxiosError).response.data}`);
+    console.error(e);
   }
 }
 
