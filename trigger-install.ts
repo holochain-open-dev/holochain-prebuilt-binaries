f86019609cae7b84ccb4aeea921baf2a0f7a4075
Matt Gabrenya <matt@buildyourweb.app>
2023-10-09 15:17:40 -0700

    refactor: use cargo install instead of cargo build -- should work on windows

---
+++ trigger-install.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import {Octokit} from "octokit";
+import { Command } from 'commander';
+import 'dotenv/config';
+
+
+
+
+async function main() {
+  const program = new Command();
+  program
+    .name('trigger')
+    .description('Trigger github action to install, build, and publish holochain prebuilt binaries')
+    .version('1.0.0')
+    .requiredOption('-c, --hc_version <string>', 'holochain_cli version')
+    .requiredOption('-h, --holochain_version <string>', 'holochain version')
+    .requiredOption('-l, --lair_version <string>', 'lair-keystore version');
+  program.parse();
+
+  const { lair_version, holochain_version, hc_version } = program.opts();
+
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_AUTH_TOKEN
+  })
+
+  try {
+    await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+      owner: 'buildyourwebapp',
+      repo: 'holochain-prebuilt-binaries',
+      event_type: `all_install_release`,
+      client_payload: {
+        lair_version,
+        holochain_version,
+        hc_version,
+      },
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    console.log(`Triggered workflow for lair-keystore v${lair_version}, holochain_cli v${hc_version}, holochain v${holochain_version}` );
+  } catch(e) {
+    //@ts-ignore
+    console.error(e);
+  }
+}
+
+main();
